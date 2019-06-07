df05ca6c982f4507849decd471484e70dd04ba1c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-06-08 01:54:19 +0300

    doc: improve descriptions of types in the `i18n` package

--- i18n/language.go
+++ i18n/language.go
@@ -6,14 +6,16 @@ import (
 )
 
 const (
-	LanguageCodeEnglish   = "en"
+	// LanguageCodeEnglish represents the English language.
+	LanguageCodeEnglish = "en"
+	// LanguageCodeBulgarian represents the Bulgarian language.
 	LanguageCodeBulgarian = "bg"
 )
 
 // Language indicates the local language (which is determined by the system locale settings).
 var Language string
 
-// InitWithLocaleName internationalization subsystem to use the appropriate local language determined by the specified locale.
+// InitWithLocaleName initializes the internationalization subsystem to use the appropriate local language as determined by the specified locale.
 func InitWithLocaleName(locale string) {
 	if strings.HasPrefix(locale, LanguageCodeBulgarian+"_") {
 		Language = LanguageCodeBulgarian
@@ -22,7 +24,7 @@ func InitWithLocaleName(locale string) {
 	}
 }
 
-// Init initializes the internationalization subsystem to use the appropriate local language defined in the system locale settings.
+// Init initializes the internationalization subsystem to use the appropriate local language as determined by the system locale settings.
 // Determining of the language from locale settings is only implemented for Unix-like platforms so far (i.e. only POSIX locales are supported). TODO: implement for Windows.
 func Init() {
 	locale := os.Getenv("LC_ALL")
